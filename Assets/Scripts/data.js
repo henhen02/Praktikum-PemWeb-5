0927be9cf411a89915fa53275d51c5deaab62194
henhen02 <hendri21072002@gmail.com>
2022-11-17 02:26:55 +0700

    Finish

---
+++ Assets/Scripts/data.js
@@ -0,0 +1,93 @@
+const Mhs = [
+    {
+        nama: 'Hendri Aldi Zulfan',
+        nim: 120140186,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Fer D. Sam',
+        nim: 123111177,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Moron Arloji',
+        nim: 121155001,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Hendra Belmawan',
+        nim: 120140180,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Fera Riantasa',
+        nim: 123111179,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Upin Bersaudara',
+        nim: 121155045,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Seno Melati',
+        nim: 120140102,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Andanan Andi',
+        nim: 123111077,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Pekor Way Halim',
+        nim: 121155122,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Riando Mekar',
+        nim: 120140002,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Joko Joki',
+        nim: 123111054,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Jordi Asia',
+        nim: 121155100,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Ichigo Yeager D Cruz',
+        nim: 120140005,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Mertani Kaya',
+        nim: 123111099,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Perkara Alba',
+        nim: 121155190,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Ridho Restu',
+        nim: 120140044,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Marry On A Cross',
+        nim: 123111023,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Mariposa Talaban',
+        nim: 121155125,
+        prodi: 'SI'
+    }
+];
+export {Mhs};
